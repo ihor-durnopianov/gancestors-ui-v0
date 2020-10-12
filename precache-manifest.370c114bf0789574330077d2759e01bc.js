0859745751c2cd953d64fd6621d5c1c2e9d303cc
Ihor Durnopianov <ihor.durnopianov@outlook.com>
2020-10-12 13:32:16 +0300

    Updates

---
+++ precache-manifest.370c114bf0789574330077d2759e01bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76281877a247af66c86f2961c4f27f98",
+    "url": "/gancestors-ui-v0/index.html"
+  },
+  {
+    "revision": "94132beb7cce444271c9",
+    "url": "/gancestors-ui-v0/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "57652319cd8dd3e961f4",
+    "url": "/gancestors-ui-v0/static/css/main.2661ccca.chunk.css"
+  },
+  {
+    "revision": "94132beb7cce444271c9",
+    "url": "/gancestors-ui-v0/static/js/2.51a57152.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gancestors-ui-v0/static/js/2.51a57152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57652319cd8dd3e961f4",
+    "url": "/gancestors-ui-v0/static/js/main.12f4244d.chunk.js"
+  },
+  {
+    "revision": "e8fa896f368f0c873ae8",
+    "url": "/gancestors-ui-v0/static/js/runtime-main.3caf0215.js"
+  }
+]);
